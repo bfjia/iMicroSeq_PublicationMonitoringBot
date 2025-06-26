6c7f09f8749a77027c2991eeac31f09923f3aa49
bfjia <bfjia@bfjia.net>
2025-06-26 10:22:07 -0700

    add command to process the output of FindScholarProfiles.py to the input of FindPublications.py

--- publii.sh
+++ publii.sh
@@ -5,6 +5,8 @@ git pull
 eval "$(conda shell.bash hook)"
 conda activate pubsurveillance
 
+cut -d$'\t' -f3 results.tsv | tail -n +2 | awk "NF" > authors.txt
+
 python ./FindPublications.py
 
 python slackConnector.py --messagefile msg.md --channel "C088LULD5PY"
