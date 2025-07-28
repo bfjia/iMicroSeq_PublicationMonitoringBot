55f6a6b8066b165152aaa52ef95035e6033918af
bfjia <bfjia@bfjia.net>
2025-07-28 09:39:25 -0700

    update the crontab entry script for finding new publications

--- publii.sh
+++ publii.sh
@@ -1,18 +1,31 @@
 #!/bin/bash -e
 
+handle_error() {
+	python slackConnector.py --message "The script errored out. Please see log attached." --file publii.log
+}
+
+trap handle_error ERR
+
 git pull
 
 eval "$(conda shell.bash hook)"
 conda activate pubsurveillance
 
+#Using the result.tsv file, format the authors.txt file. 
 cut -d$'\t' -f3 results.tsv | tail -n +2 | awk "NF" > authors.txt
 
+echo "There are $(wc -l < authors.txt) authors in the list to monitor."
+
+#Find publications
 python ./FindPublications.py
 
-python slackConnector.py --messagefile msg.md --channel "C088LULD5PY"
+#Post the message into Slack
+python slackConnector.py --messagefile msg.md --channel "C097CKA5U4X"
 
 dateNow=$(date +%Y%m%d)
 
-git add .
-git commit -m "Update $dateNow"
-git push
+#git add .
+#git commit -m "Update $dateNow"
+#git push
+
+conda deactivate
